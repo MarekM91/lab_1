49ecea5022c717c666088d540f224959cf2e4ef7
Michał <mozi1991@gmail.com>
2021-09-19 12:16:42 +0200

    Wykonanie ćwiczenia 1

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,21 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Witam w ćwiczeniu nr1.");
+        System.out.println("Ćwiczę w nim wypisywanie tekstu w konsoli.");
 
+        System.out.printf("Mam na imię %s, i uczę się programowania w języku %s. \n", "Michał", "JAVA");
+        System.out.printf("Są to %d zajęcia.", 1);
+
+        System.out.print("Wypróbowałem 3 dostępnych funkcji \n");
+
+        System.out.print("print");
+        System.out.print(" - wypisuje argumenty na ekranie \n");
+
+        System.out.print("println");
+        System.out.print(" - wypisuje argumenty na ekranie i następnie ustawia kursor na początek kolejnego wiersza \n");
+
+        System.out.print("printf");
+        System.out.print(" - umożliwia wypisanie sformatowanego tekstu \n");
     }
 }
