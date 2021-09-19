72ee9a0c3ec9da8eb5016a2ca0e5c8f9dd338d04
Michał <mozi1991@gmail.com>
2021-09-19 14:01:02 +0200

    Wykonanie ćwiczenia 6

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,12 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
+        double[] tablica = {1,2,3,5,10};
+        System.out.println("Pozycja 1 z tablicy : " + tablica[0]);
+        System.out.println("Pozycja 2 z tablicy : " + tablica[1]);
+        System.out.println("Pozycja 3 z tablicy : " + tablica[2]);
+        System.out.println("Pozycja 4 z tablicy : " + tablica[3]);
+        System.out.println("Pozycja 5 z tablicy : " + tablica[4]);
 
     }
 }
